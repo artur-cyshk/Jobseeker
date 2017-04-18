ef8c665366a8e4f7cdbf5aa00202942ea97d9d0a
Artur Viachorka <artur.viachorka@itechart-group.com>
2017-04-18 12:09:53 +0300

    remove unnecessary routes

--- server/application/router/router.js
+++ server/application/router/router.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const auth = require("../../configuration/jwt/jwtAuth.js")();  
-router.get('/logout', auth.authenticate(), require('./routes/logout'));
-router.get('/authorized', require('./routes/authorized'));
+// auth.authenticate() - set second param to router functions if route need authorized user
 router.post('/login', require('./routes/login'));
 router.post('/registration', require('./routes/registration'));
 module.exports = router;
--- server/application/router/routes/authorized.js
+++
@@ -1,4 +0,0 @@
-var connection = require('../../../configuration/database/connection');
-module.exports = function (req, res, next) {
-	res.end();
-};
